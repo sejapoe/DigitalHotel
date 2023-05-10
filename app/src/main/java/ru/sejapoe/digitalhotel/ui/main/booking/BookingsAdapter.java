4ec9ecb04b1bdd1688b896fddf822b87510ce864
sejapoe <bekaldiev2015@gmail.com>
2023-05-10 23:14:11 +0300

    improve design

--- app/src/main/java/ru/sejapoe/digitalhotel/ui/main/booking/BookingsAdapter.java
+++ app/src/main/java/ru/sejapoe/digitalhotel/ui/main/booking/BookingsAdapter.java
@@ -1,6 +1,10 @@
 package ru.sejapoe.digitalhotel.ui.main.booking;
 
+import android.view.View;
+
 import androidx.annotation.NonNull;
+
+import java.time.LocalDate;
 
 import ru.sejapoe.digitalhotel.R;
 import ru.sejapoe.digitalhotel.data.model.hotel.booking.Booking;
@@ -18,6 +22,14 @@ public class BookingsAdapter extends ClickableItemListAdapter<BookingItemBinding
         binding.hotelName.setText(booking.getHotel().getName());
         binding.roomType.setText(booking.getRoomType().getName());
         binding.price.setText(binding.getRoot().getResources().getString(R.string.price, booking.getRoomType().getPrice()));
+        binding.price.setVisibility(booking.getPayment() != null ? View.GONE : View.VISIBLE);
         binding.dates.setText(binding.getRoot().getResources().getString(R.string.dates, booking.getCheckInDate(), booking.getCheckOutDate()));
+        LocalDate today = LocalDate.now();
+        if (booking.getCheckInDate().isAfter(today)) {
+            binding.daysLeft.setText(binding.getRoot().getResources().getString(R.string.days_left, booking.getCheckInDate().toEpochDay() - today.toEpochDay()));
+        }
+        if (booking.getCheckInDate().isEqual(today)) {
+            binding.daysLeft.setText(R.string.today);
+        }
     }
 }
